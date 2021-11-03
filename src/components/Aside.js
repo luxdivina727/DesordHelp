8b2bd71e719c2335fe9d148d50814ed6c45c645a
Luz Divina <luxdivina727@gmail.com>
2021-11-03 08:52:42 -0500

    ** LOGIN MODIFICACIÓN **

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -4,7 +4,7 @@ export default function Aside() {
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       {/* Brand Logo */}
-      <a href="index3.html" className="brand-link">
+      <a href="#" className="brand-link">
         <img
           src="dist/img/AdminLTELogo.png"
           alt="AdminLTE Logo"
@@ -19,7 +19,7 @@ export default function Aside() {
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
           <div className="image">
             <img
-              src="dist/img/user2-160x160.jpg"
+              src="dist/img/avatar3.png"
               className="img-circle elevation-2"
               alt="User Image"
             />
