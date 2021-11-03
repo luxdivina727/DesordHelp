01a539c56f81f62c6b540ab754bfc43a4f2137c0
Luz Divina <luxdivina727@gmail.com>
2021-11-02 20:47:03 -0500

    MasterPage DesordHelp

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export default function Content() {
+  return (
+    <Fragment>
+      <div className="content-wrapper">Content</div>
+    </Fragment>
+  );
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="main-footer">
+      <strong>
+        Desorhelp.
+      </strong>
+      <div className="float-right d-none d-sm-inline-block">
+        <b>Version</b> Alpha
+      </div>
+    </footer>
+  );
+}
